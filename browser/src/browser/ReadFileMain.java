9be09ef9730a3812a72c117cf86f5a35cd3fe711
Snapdragon4815162342 <61626937+Snapdragon4815162342@users.noreply.github.com>
2020-05-16 11:24:12 +0200

    dtfghbnjmk

---
+++ browser/src/browser/ReadFileMain.java
@@ -0,0 +1,13 @@
+package browser;
+
+import javax.swing.JFrame;
+
+
+public class ReadFileMain {
+
+	public static void main(String[] args) {
+		ReadFile dude = new ReadFile();
+		dude.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
